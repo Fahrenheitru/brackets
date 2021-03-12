e8fa3cb01f6c8c3e10b78d2c2d7f9e302f2be7d9
Fahrenheitru <ifahrenheitru@gmail.com>
2021-03-12 19:42:42 +0500

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let arr = bracketsConfig;
+    let ch = str.split("");
+    let arrStr = arr
+        .join("")
+        .split("")
+        .filter(function (value) {
+            return !(value === ",");
+        });
+    let stack = [];
+    let openBr = [];
+    let closeBr = [];
+    let closeInd, openInd;
+    openBr = arrStr.filter(function (value, index) {
+        return index % 2 === 0;
+    });
+    closeBr = arrStr.filter(function (value, index) {
+        return index % 2 === 1;
+    });
+    for (let i = 0; i < ch.length; i++) {
+        openInd = openBr.indexOf(ch[i]);
+        if (openInd !== -1) {
+            stack.unshift(openInd);
+        }
+        closeInd = closeBr.indexOf(ch[i]);
+        if (closeInd !== -1) {
+            openInd = stack.shift();
+            if (closeInd !== openInd) {
+                return false;
+            }
+        }
+    }
+    if (stack.length !== 0) {
+        return false;
+    }
+    return true;
+};
